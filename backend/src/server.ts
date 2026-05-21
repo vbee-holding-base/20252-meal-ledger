2546922bbe531be27d51df4e47a07d49501b0bb5
Nguyễn Kim Huy <nkhuy05@gmail.com>
2026-05-21 09:48:15 +0700

    feat: implement initial backend configuration with database connection and server setup

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import cors from "cors";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+connectDB();
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT ?? 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
