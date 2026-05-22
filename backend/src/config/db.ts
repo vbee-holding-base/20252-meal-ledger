0dea958a4320018ae9fd3bdc4f5e9a27c8743d73
Nhat Minh Nguyen Tran <nhatminhnt06@gmail.com>
2026-05-22 16:22:46 +0700

    feat: creating CRUD operation on restaurants

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect("mongodb+srv://nhatminhnt06_db_user:ceeuSajOxHRYz3Lb@cluster0.bgazxry.mongodb.net/test?appName=Cluster0");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
-
+connectDB();
 export default connectDB;
