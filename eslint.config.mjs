4d2b3a59422281d265eaf25b4877f95c444fe864
levinh2006b <ledinhvinh7a2@gmail.com>
2026-05-22 17:35:58 +0700

    feat: homepage, participants, restaurants, mobile ui layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,25 +4,44 @@ import json from "@eslint/json";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    ignores: [
+      "dist",
+      "node_modules",
+      "backend",
+      "tsconfig.json",
+      "tsconfig.*.json",
+    ],
+  },
+  ...tseslint.configs.recommended.map((config) => ({
+    ...config,
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+  })),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    rules: js.configs.recommended.rules,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
   {
     files: ["**/*.json"],
     plugins: { json },
     language: "json/json",
-    extends: ["json/recommended"],
+    rules: {
+      "json/no-duplicate-keys": "error",
+    },
   },
   {
     files: ["**/*.css"],
     plugins: { css },
     language: "css/css",
-    extends: ["css/recommended"],
   },
   eslintConfigPrettier,
 ]);
