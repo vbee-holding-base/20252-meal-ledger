3dca273006021df2908d13c41fa1da6fc8556f57
Nguyễn Kim Huy <nkhuy05@gmail.com>
2026-05-22 21:48:41 +0700

    Revert "fix: login"
    This reverts commit 3e378b5811fe73ac41965b8c7333ec2117210aed.

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string, { family: 4 });
+    await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
