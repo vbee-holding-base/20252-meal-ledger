04a0881ff26c54d2211adb0c2b833e9026b02b02
levinh2006b <ledinhvinh7a2@gmail.com>
2026-05-22 18:08:03 +0700

    fix: login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,12 +4,12 @@ dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGODB_URI as string, { family: 4 });
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
-connectDB();
+
 export default connectDB;
