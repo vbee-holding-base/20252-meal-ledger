3e378b5811fe73ac41965b8c7333ec2117210aed
levinh2006b <ledinhvinh7a2@gmail.com>
2026-05-22 18:08:03 +0700

    fix: login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(process.env.MONGODB_URI as string, { family: 4 });
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
